6098779bccbca4a585a9c90252e923babc904874
yutohub <83153839+yutohub@users.noreply.github.com>
2023-07-02 21:13:47 +0900

    fix subtitle path

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function onPlayerReady(event) {
 
 // 字幕ファイルの存在確認
 function checkSubtitleFile(videoId) {
-    const subtitleUrl = `https://github.com/yutohub/transcribify/blob/main/subtitles/${videoId}.json`;
+    const subtitleUrl = `https://raw.githubusercontent.com/yutohub/transcribify/main/subtitles/${videoId}.json`;
     fetch(subtitleUrl)
       .then(response => {
         if (response.ok) {
@@ -113,7 +113,8 @@ function getVideoIdFromUrl(url) {
 
 // TSVファイルを読み込む
 async function fetchData(videoId) {
-    const data = await fetch("https://github.com/yutohub/transcribify/blob/main/subtitles/" + videoId + ".json");
+    const subtitleUrl = `https://raw.githubusercontent.com/yutohub/transcribify/main/subtitles/${videoId}.json`;
+    const data = await fetch(subtitleUrl);
     subtitles = await data.json();
   }
 
