c24d995a2137121785c4979936f57800916d887d
yutohub <83153839+yutohub@users.noreply.github.com>
2023-07-03 17:12:18 +0900

    add subtitle xCS76NGj3u4

--- main.js
+++ main.js
@@ -159,14 +159,15 @@ function padZero(number) {
 }
 
 // --- サムネイルを表示する --- //
-
 // YouTube video IDs
 const videoIds = [
   "54UThDl00qI", "bZQun8Y4L2A", "cdiD-9MMpb0",
   "g2R2T631x7k", "kCc8FmEb1nY", "Le6c4ZaFyAE",
   "pjc_oo4ApSY", "qKBubKO-798", "RcYjXbSJBN8",
-  "Rty_HFuyG4I", "t3YJ5hKiMQ0", "ycPr5-27vSI"
+  "Rty_HFuyG4I", "t3YJ5hKiMQ0", "ycPr5-27vSI",
+  "xCS76NGj3u4"
 ];
+
 const thumbnailUrls = videoIds.map(id => `http://img.youtube.com/vi/${id}/mqdefault.jpg`);
 const thumbnailsContainer = document.getElementById("thumbnails-container");
 thumbnailUrls.forEach(url => {
