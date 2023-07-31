a8287fa8dddedc41aca0a1ec1b0106ca6ddbcfd6
yutohub <83153839+yutohub@users.noreply.github.com>
2023-08-01 02:16:55 +0900

    add subtitle sYyVi-H-ozI

--- main.js
+++ main.js
@@ -167,7 +167,8 @@ const videoIds = [
   "Rty_HFuyG4I", "t3YJ5hKiMQ0", "ycPr5-27vSI",
   "xCS76NGj3u4", "Q45sr5p_NmQ", "Df_2BBTvJ2o",
   "wD14hb4d04c", "_EfEoSP7oYQ", "umhl2hakkcY",
-  "f4NOJ42-BKM", "feQhHStBVLE", "rJKN_880b-M"
+  "f4NOJ42-BKM", "feQhHStBVLE", "rJKN_880b-M",
+  "sYyVi-H-ozI"
 ];
 
 const thumbnailUrls = videoIds.map(id => `http://img.youtube.com/vi/${id}/mqdefault.jpg`);
